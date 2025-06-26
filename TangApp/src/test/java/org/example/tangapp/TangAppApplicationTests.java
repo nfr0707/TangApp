1407c8e2d1e984708df943ff1b0ae189b070b282
nfr0707 <2240690872@qq.com>
2025-06-26 09:15:57 +0800

    Add files via upload

---
+++ TangApp/src/test/java/org/example/tangapp/TangAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.tangapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TangAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
