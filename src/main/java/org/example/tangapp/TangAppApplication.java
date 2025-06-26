4703550e97ebadc3e56beca513c42118fb41c5d4
nfr0707 <2240690872@qq.com>
2025-06-26 10:45:58 +0800

    Add files via upload

---
+++ src/main/java/org/example/tangapp/TangAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.tangapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TangAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TangAppApplication.class, args);
+    }
+
+}
